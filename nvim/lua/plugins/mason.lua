104b15ced5759e71135bf53a43b511116dab4a92
Dieg0407 <diego.pastor.0407@gmail.com>
2024-03-18 22:02:09 -0500

    feat: added basic lsp configuration

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function(PlugSpec, opts)
+      require("mason").setup(opts)
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "clangd", "terraformls" }
+    },
+    config = function(PlugSpec, opts)
+      require("mason-lspconfig").setup(opts)
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim"
+    },
+    config = function()
+      local lspconfig = require('lspconfig')
+      lspconfig.rust_analyzer.setup {}
+      lspconfig.lua_ls.setup {}
+      lspconfig.clangd.setup {}
+      lspconfig.terraformls.setup {}
+      lspconfig.tsserver.setup {}
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', '<Leader>d', vim.lsp.buf.hover)
+          vim.keymap.set('n', '<Leader>gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', '<Leader>gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<Leader>k', vim.lsp.buf.signature_help, opts)
+
+          vim.keymap.set('n', '<Leader>td', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', '<Leader>gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<Leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+        end,
+      })
+    end,
+  }
+}
