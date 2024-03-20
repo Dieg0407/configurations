c15e3cb4d05f43bebfce855926dd3f90c16efb3f
Dieg0407 <diego.pastor.0407@gmail.com>
2024-03-19 21:54:36 -0500

    feat: added cmp

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,127 @@
+local function setup_cmp(cmp)
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered()
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' },
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+end
+
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function(_, opts)
+      require("mason").setup(opts)
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "clangd", "terraformls" }
+    },
+    config = function(_, opts)
+      require("mason-lspconfig").setup(opts)
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip"
+    },
+    config = function()
+      local cmp = require("cmp")
+      setup_cmp(cmp)
+
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+      local capabilities = cmp_nvim_lsp.default_capabilities()
+
+      local lspconfig = require('lspconfig')
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }
+            }
+          }
+        }
+      }
+      lspconfig.rust_analyzer.setup { capabilities = capabilities }
+      lspconfig.clangd.setup { capabilities = capabilities }
+      lspconfig.terraformls.setup { capabilities = capabilities }
+      lspconfig.tsserver.setup { capabilities = capabilities }
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', '<Leader>d', vim.lsp.buf.hover)
+          vim.keymap.set('n', '<Leader>gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', '<Leader>gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<Leader>k', vim.lsp.buf.signature_help, opts)
+
+          vim.keymap.set('n', '<Leader>td', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', '<Leader>gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<Leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+        end,
+      })
+    end,
+  }
+}
