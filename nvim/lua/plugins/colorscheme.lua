a1ec3a2caa5cbad37d18be6851bcb9b7c83f83f4
Dieg0407 <diego.pastor.0407@gmail.com>
2024-06-01 00:09:38 -0500

    chore: changed colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,24 @@
+--  {
+--    "folke/tokyonight.nvim",
+--    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+--    priority = 1000, -- make sure to load this before all the other start plugins
+--    config = function()
+--      -- load the colorscheme here
+--      vim.cmd([[colorscheme tokyonight-night]])
+--    end,
+--  }
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "Shatur/neovim-ayu",
+    lazy = false,
     config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight-night]])
-    end,
+      require("ayu").setup({
+        overrides = {
+          Normal = { bg = "None" }
+        }
+      })
+
+      vim.cmd.colorscheme "ayu"
+    end
   }
 }
