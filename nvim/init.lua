8c1de278f695afc6af2b40b5196421fcf4504a3f
Dieg0407 <diego.pastor.0407@gmail.com>
2024-07-18 22:22:38 -0500

    added tab size per file in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,14 @@
+local function set_tab_size(filetype, tabsize)
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = filetype,
+    callback = function()
+      vim.bo.tabstop = tabsize
+      vim.bo.shiftwidth = tabsize
+      vim.bo.expandtab = true -- Set to false if you prefer tabs instead of spaces
+    end,
+  })
+end
+
 -- setup base key bindings and settings
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
@@ -7,6 +18,11 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.bo.softtabstop = 4
+
+set_tab_size("python", 4)
+set_tab_size("javascript", 2)
+set_tab_size("html", 2)
+set_tab_size("lua", 2)
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
