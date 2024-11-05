a3b0ed0fb6b4818fc04e4b2ffafa3fcba9e1c10e
Dieg0407 <diego.pastor.0407@gmail.com>
2024-11-04 19:52:32 -0500

    chore: added jenkins installation script

--- debian-server-setup.sh
+++ debian-server-setup.sh
@@ -86,3 +86,16 @@ then
 else
 	echo "Nginx is installed."
 fi
+
+# jenkins
+if ! command -v jenkins &> /dev/null
+then
+	sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+	echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+	  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+	  /etc/apt/sources.list.d/jenkins.list > /dev/null
+	sudo apt-get update -y
+	sudo apt-get install jenkins -y
+else
+	echo "Jenkins is installed."
+fi
