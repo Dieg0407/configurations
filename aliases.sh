a52d1239d0b3866c2f953919167ea7da7fd6c4da
Dieg0407 <diego.pastor.0407@gmail.com>
2024-12-27 18:28:48 -0500

    chore: added aliases file

---
+++ aliases.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# some aliases
+## git
+alias gr="git restore --staged"
+alias gaa="git add ."
+alias gcv="git commit -v"
+alias gps="git push"
+alias gpl="git pull"
+alias gl="git log --oneline"
+alias gs="git status"
+alias gst="git stash"
+alias gt="git tag"
+
+## linux
+alias schown="sudo chown"
